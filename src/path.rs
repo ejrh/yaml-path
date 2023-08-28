a0d248cc571b5f5b64b98b33f0c5ef708e313c2e
Edmund Horner <ejrh00@gmail.com>
2023-08-28 20:43:31 +1200

    Improve splitting a path into segments

--- src/path.rs
+++ src/path.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 use yaml_rust::Yaml;
 use crate::PathError;
+use crate::PathError::ParseError;
 use crate::segment::Segment;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -42,11 +43,14 @@ impl Separator {
 impl Path {
     pub fn new(path_str: &str) -> Result<Path, PathError> {
         let original = String::from(path_str);
-        let separator = detect_separator(path_str);
         let mut segments = Vec::new();
-        let path_str = path_str.strip_prefix('/').unwrap_or(path_str);
-        for part in path_str.split(separator.symbol()) {
-            if part == "*" {
+
+        let (segment_parts, separator) = segment_parts(path_str)?;
+
+        for part in segment_parts {
+            if part.is_empty() {
+                return Err(ParseError);
+            } else if part == "*" {
                 segments.push(Segment::Wildcard);
             } else {
                 let key = i64::from_str(part)
@@ -87,6 +91,35 @@ impl Path {
     }
 }
 
+fn segment_parts(path_str: &str) -> Result<(Vec<&str>, Separator), PathError> {
+    let separator = detect_separator(path_str);
+    let path_str = path_str.strip_prefix('/').unwrap_or(path_str);
+
+    let mut parts = Vec::new();
+    let mut start_pos = 0;
+
+    let mut new_part = |start_pos, end_pos| {
+        if end_pos == start_pos {
+            return;
+        }
+        parts.push(&path_str[start_pos..end_pos]);
+    };
+
+    for (pos, ch) in path_str.chars().enumerate() {
+        if ch == separator.symbol() {
+            new_part(start_pos, pos);
+            start_pos = pos + 1;
+        } else if ch == '/' || ch == '.' {
+            return Err(ParseError);
+        }
+    }
+    new_part(start_pos, path_str.len());
+    if parts.is_empty() {
+        return Err(ParseError);
+    }
+    return Ok((parts, separator));
+}
+
 #[cfg(test)]
 mod test {
     use crate::PathError::ParseError;
@@ -103,12 +136,21 @@ mod test {
 
     #[test]
     fn test_bad_path_separators() {
-        //assert_eq!(Err(ParseError), Path::new(""));
-        //assert_eq!(Err(ParseError), Path::new("/"));
-        //assert_eq!(Err(ParseError), Path::new("."));
-        //assert_eq!(Err(ParseError), Path::new("foo/bar"));
-        //assert_eq!(Err(ParseError), Path::new("/foo.bar"));
-        //assert_eq!(Err(ParseError), Path::new("foo.bar/baz"));
+        assert_eq!(Err(ParseError), Path::new(""));
+        assert_eq!(Err(ParseError), Path::new("/"));
+        assert_eq!(Err(ParseError), Path::new("."));
+        assert_eq!(Err(ParseError), Path::new("foo/bar"));
+        assert_eq!(Err(ParseError), Path::new("/foo.bar"));
+        assert_eq!(Err(ParseError), Path::new("foo.bar/baz"));
+    }
+
+    #[test]
+    fn test_segment_parts() {
+        assert_eq!(Err(ParseError), segment_parts(""));
+        assert_eq!(Err(ParseError), segment_parts("/"));
+        assert_eq!(Ok((vec!["a"], Separator::Dot)), segment_parts("a"));
+        assert_eq!(Ok((vec!["a", "b"], Separator::Slash)), segment_parts("/a/b"));
+        assert_eq!(Ok((vec!["a", "b"], Separator::Dot)), segment_parts("a.b"));
     }
 
     #[test]
